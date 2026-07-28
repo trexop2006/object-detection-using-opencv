a575a7c625bdb1a675bd9d2e132ff292ab9b59fe
Trex <chaturvedihardik097@gmail.com>
2026-07-28 17:12:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -129,5 +129,19 @@ webrtc_streamer(
     media_stream_constraints={
         "video": True,
         "audio": False
+    },
+    webrtc_streamer(
+    key="object-detection",
+    video_processor_factory=VideoProcessor,
+    media_stream_constraints={
+        "video": True,
+        "audio": False
+    },
+    rtc_configuration={
+        "iceServers": [
+            {"urls": ["stun:stun.l.google.com:19302"]}
+        ]
     }
 )
+)
+
